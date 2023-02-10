6634f054b197ea4999dac9b41fbc0591abb71949
Maxwell Twardowski <maxtwardo@gmail.com>
2023-02-10 17:33:32 -0500

    Edited laser script

--- IronmanProj/Assets/FireLaser.cs
+++ IronmanProj/Assets/FireLaser.cs
@@ -3,9 +3,6 @@ using Valve.VR;
 
 public class FireLaser : MonoBehaviour
 {
-
-// add laser orientation & velocity, despawn on collision
-
     public GameObject laserPrefab;
     private SteamVR_Input_Sources leftGrip;
     private SteamVR_Input_Sources rightGrip;
@@ -20,18 +17,27 @@ public class FireLaser : MonoBehaviour
 
     private void Update()
     {
-        if (gripAction.GetStateDown(rightGrip))
-        {
+	if (gripAction.GetStateDown(leftGrip) && gripAction.GetStateDown(rightGrip)) {
+	    // CREATE UNIBEAM ORIGINATING FROM PLAYER CHEST
+		// Can create GameObject unibeamPrefab similar to laserPrefab
+ 	    // DEDUCT AN AMOUNT FROM ENERGY BAR, ENSURE ENOUGH ENERGY REMAINS
+	}
+	else if (gripAction.GetStateDown(leftGrip)) {	//need to add velocity, orientation, bind spawning
             Vector3 spawnPosition = transform.position + transform.forward;
             Quaternion spawnRotation = Quaternion.identity;
             GameObject cylinder = Instantiate(laserPrefab, spawnPosition, spawnRotation);
+	    //* DEDUCT AN AMOUNT FROM ENERGY BAR, ENSURE ENOUGH ENERGY REMAINS *//
+        }
+        else if (gripAction.GetStateDown(rightGrip)) {	//need to add velocity, orientation, bind spawning
+            Vector3 spawnPosition = transform.position + transform.forward;
+            Quaternion spawnRotation = Quaternion.identity;
+            GameObject cylinder = Instantiate(laserPrefab, spawnPosition, spawnRotation);
+	    //* DEDUCT AN AMOUNT FROM ENERGY BAR, ENSURE ENOUGH ENERGY REMAINS *//
         }
 
-	if (gripAction.GetStateDown(leftGrip))
-        {
-            Vector3 spawnPosition = transform.position + transform.forward;
-            Quaternion spawnRotation = Quaternion.identity;
-            GameObject cylinder = Instantiate(laserPrefab, spawnPosition, spawnRotation);
-        }
+	
+	//* Add if statement reading if (left trigger?) is pressed, spawn sword bound to Player->SteamVRObjects->LeftHand(If we use left trigger)->ObjectAttachmentPoint *//
+	//* Will need to check if the sword should spawn or despawn, i.e. if sword exists despawn on click, if sword does not exist spawn on click *//
+
     }
 }
