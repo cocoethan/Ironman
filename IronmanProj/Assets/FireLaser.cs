f25fb9a617c72bd3b75bbee8ef3d878272cd5570
Maxwell Twardowski <maxtwardo@gmail.com>
2023-02-09 20:04:01 -0500

    lasers made, robots interactable

--- IronmanProj/Assets/FireLaser.cs
+++ IronmanProj/Assets/FireLaser.cs
@@ -4,18 +4,27 @@ using Valve.VR;
 public class FireLaser : MonoBehaviour
 {
     public GameObject laserPrefab;
-    private SteamVR_Input_Sources handType;
+    private SteamVR_Input_Sources leftGrip;
+    private SteamVR_Input_Sources rightGrip;
     private SteamVR_Action_Boolean gripAction;
 
     private void Awake()
     {
-        handType = SteamVR_Input_Sources.RightHand;
+        leftGrip = SteamVR_Input_Sources.LeftHand;
+        rightGrip = SteamVR_Input_Sources.RightHand;
         gripAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("GrabGrip");
     }
 
     private void Update()
     {
-        if (gripAction.GetStateDown(handType))
+        if (gripAction.GetStateDown(rightGrip))
+        {
+            Vector3 spawnPosition = transform.position + transform.forward;
+            Quaternion spawnRotation = Quaternion.identity;
+            GameObject cylinder = Instantiate(laserPrefab, spawnPosition, spawnRotation);
+        }
+
+	if (gripAction.GetStateDown(leftGrip))
         {
             Vector3 spawnPosition = transform.position + transform.forward;
             Quaternion spawnRotation = Quaternion.identity;
