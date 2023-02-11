48127c330bd6f9a0797168c46a73c4aec1eab375
Maxwell Twardowski <maxtwardo@gmail.com>
2023-02-10 21:58:46 -0500

    physics and sound added to lasers

--- IronmanProj/Assets/FireLaser.cs
+++ IronmanProj/Assets/FireLaser.cs
@@ -3,13 +3,12 @@ using Valve.VR;
 
 public class FireLaser : MonoBehaviour
 {
-
-// add laser orientation & velocity, despawn on collision
-
     public GameObject laserPrefab;
     private SteamVR_Input_Sources leftGrip;
     private SteamVR_Input_Sources rightGrip;
     private SteamVR_Action_Boolean gripAction;
+    public AudioClip fireSound;
+    private AudioSource audioSource;
 
     private void Awake()
     {
@@ -18,20 +17,30 @@ public class FireLaser : MonoBehaviour
         gripAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("GrabGrip");
     }
 
+    void Start() { 
+	audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = fireSound;
+    }
+
     private void Update()
     {
-        if (gripAction.GetStateDown(rightGrip))
-        {
+	if (gripAction.GetStateDown(leftGrip)) {						// * May need to say && !right if the lasers fire with the unibeam
+            Vector3 spawnPosition = transform.position + transform.forward;			// likely will be wonky
+            Quaternion spawnRotation = Quaternion.identity;	
+	    audioSource.Play();									// may need to be 0 ^
+            GameObject cylinder = Instantiate(laserPrefab, spawnPosition, spawnRotation);
+	    //* DEDUCT AN AMOUNT FROM ENERGY BAR, ENSURE ENOUGH ENERGY REMAINS *//
+        }
+        if (gripAction.GetStateDown(rightGrip)) {
             Vector3 spawnPosition = transform.position + transform.forward;
             Quaternion spawnRotation = Quaternion.identity;
+            audioSource.Play();
             GameObject cylinder = Instantiate(laserPrefab, spawnPosition, spawnRotation);
+	    //* DEDUCT AN AMOUNT FROM ENERGY BAR, ENSURE ENOUGH ENERGY REMAINS *//
         }
 
-	if (gripAction.GetStateDown(leftGrip))
-        {
-            Vector3 spawnPosition = transform.position + transform.forward;
-            Quaternion spawnRotation = Quaternion.identity;
-            GameObject cylinder = Instantiate(laserPrefab, spawnPosition, spawnRotation);
-        }
+	//* Add if statement reading if (left trigger?) is pressed, spawn sword bound to Player->SteamVRObjects->LeftHand(If we use left trigger)->ObjectAttachmentPoint *//
+	//* Will need to check if the sword should spawn or despawn, i.e. if sword exists despawn on click, if sword does not exist spawn on click *//
+
     }
 }
