aa0243a2db091159c757d3bb532cf320eab86068
Maxwell Twardowski <maxtwardo@gmail.com>
2023-02-12 13:57:33 -0500

    lasers

--- IronmanProj/Assets/LaserPhysics.cs
+++ IronmanProj/Assets/LaserPhysics.cs
@@ -5,35 +5,19 @@ using Valve.VR;
 
 public class LaserPhysics : MonoBehaviour
 {
-
-    GameObject hand;
-    SteamVR_Input_Sources leftGrip;
-    SteamVR_Input_Sources rightGrip;
+    GameObject player;
     SteamVR_Action_Boolean gripAction;
-
-    void Awake()
-    {
-        leftGrip = SteamVR_Input_Sources.LeftHand;
-        rightGrip = SteamVR_Input_Sources.RightHand;
-        gripAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("GrabGrip");
-    }
 
     void Start()
     {
-	if (gripAction.GetStateDown(leftGrip)) {		// may not work
-            hand = GameObject.Find("LeftHand");
-    }
-    if (gripAction.GetStateDown(rightGrip)) {
-            hand = GameObject.Find("RightHand");
-    }
-	//else{ hand = GameObject.Find("Player");}
-        this.gameObject.transform.LookAt(hand.transform);
-	    this.gameObject.transform.Translate((Vector3.forward * 5.0f * Time.smoothDeltaTime)); //25
+        player = GameObject.Find("VRCamera");
+        this.gameObject.transform.LookAt(player.transform);
+	    this.gameObject.transform.Translate(-(Vector3.forward * 5.0f * Time.smoothDeltaTime)); //25
     }
 
     void Update()
     {
-	    this.gameObject.transform.Translate((Vector3.forward * 5.0f * Time.smoothDeltaTime));
+	    this.gameObject.transform.Translate(-(Vector3.forward * 5.0f * Time.smoothDeltaTime));
     }
 
     void OnCollisionEnter() {
