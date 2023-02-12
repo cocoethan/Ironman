7ececdf4bb6433e3a1222f5537c5c19724119a7f
Maxwell Twardowski <maxtwardo@gmail.com>
2023-02-12 16:51:56 -0500

    asdf

---
+++ IronmanProj/Assets/UnibeamPhysics.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class UnibeamPhysics : MonoBehaviour
+{
+    GameObject player;
+    SteamVR_Action_Boolean gripAction;
+
+    void Start()
+    {
+        player = GameObject.Find("VRCamera");
+        this.gameObject.transform.LookAt(player.transform);
+        this.gameObject.transform.Translate(-(Vector3.forward * 5.0f * Time.smoothDeltaTime)); //25
+    }
+
+    void Update()
+    {
+        this.gameObject.transform.Translate(-(Vector3.forward * 5.0f * Time.smoothDeltaTime));
+    }
+
+    void OnCollisionEnter()
+    {
+        Destroy(this.gameObject, 0.0f);
+    }
+}
